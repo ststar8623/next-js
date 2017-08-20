21e00c5f50f843cd4cb41e20fd53cf0a6768c632
ststar8623 <ststar8623@gmail.com>
2017-08-20 15:22:41 -0700

    Create style and next routing

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import Link from 'next/link';
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      {/* <a style={{ fontSize: 20 }}>About Page</a> */}
+      <button>Go to About Page</button>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 )
