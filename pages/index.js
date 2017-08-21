a697dc1139fd86b8fbec026c74df9831916c64b5
ststar8623 <ststar8623@gmail.com>
2017-08-20 20:52:39 -0700

    Styling components

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,46 @@ import Layout from '../components/MyLayout';
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
 
+const ShowLink = ({ show }) => (
+  <li key={show.id}>
+    <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+      <a>{show.name}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }    
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
+)
+
 const Index = (props) => (
   <Layout>
     <h1>Batman TV Shows</h1>
     <ul>
       {props.shows.map(({show}) => (
-        <li key={show.id}>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
+        <ShowLink key={show.id} show={show} />
       ))}
     </ul>
+    <style jsx>{`
+      h1, a {
+        font-family: 'Arial';
+      }
+
+      ul {
+        padding: 0;
+      }
+    `}</style>
   </Layout>
 )
 
